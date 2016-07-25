52f39de0ca89425eb7fd79faa56d254d4ca1c6a4
mohan0611 <mohan0611@gmail.com>
2016-07-25 14:26:22 +0530

    Code initial

--- SampleMVCAngular/Scripts/app/mynewcontroller.js
+++ SampleMVCAngular/Scripts/app/mynewcontroller.js
@@ -1,4 +1,4 @@
-﻿angular.module('QuizApp')
+﻿angular.module('QuizApp', [])
     .controller('QuizCtrl', function ($scope, $http) {
         $scope.answered = false;
         $scope.title = "loading question...";
